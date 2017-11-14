a82886286da5a4bf87129d9fe7a0c21df0a84908
Jeremy Felt <jeremy.felt@gmail.com>
2017-11-13 16:32:12 -0800

    Add event component output using new card structure

---
+++ components/event/content.php
@@ -0,0 +1,32 @@
+<?php
+
+$post_share_placement = spine_get_option( 'post_social_placement' );
+
+?>
+<article id="event-<?php the_ID(); ?>" class="card card--event">
+
+	<header class="card-title">
+		<?php
+
+		if ( is_single() ) {
+			?><h1><?php the_title(); ?></h1><?php
+		} else {
+			?><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2><?php
+		}
+		?>
+	</header>
+
+	<div class="card-content">
+		<?php the_content(); ?>
+	</div>
+
+	<?php
+	if ( spine_has_featured_image() ) {
+		?>
+		<figure class="card-image">
+			<?php the_post_thumbnail( 'spine-small_size' ); ?>
+		</figure>
+		<?php
+	}
+	?>
+</article>
