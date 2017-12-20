aee8a5732262b2ec179337c2e1d7f35e7cd7fca1
Phil Cable <pcable@wsu.edu>
2017-12-20 11:51:49 -0800

    Replace `paginate_links` with `the_posts_navigation`
    Unless I'm missing something obvious, this works just fine. And it
    doesn't throw a PHPCS linting error :)

--- archive-event.php
+++ archive-event.php
@@ -26,23 +26,18 @@ get_header();
 		</div><!--/column two-->
 
 	</section>
-	<?php
-
-	/* @type WP_Query $wp_query */
-	global $wp_query;
-
-	$big = 99164;
-	$args = array(
-		'base'         => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-		'format'       => 'page/%#%',
-		'total'        => $wp_query->max_num_pages, // Provide the number of pages this query expects to fill.
-		'current'      => max( 1, get_query_var( 'paged' ) ), // Provide either 1 or the page number we're on.
-	);
-	?>
 	<footer class="main-footer archive-footer">
 		<section class="row side-right pager prevnext gutter">
 			<div class="column one">
-				<?php echo paginate_links( $args ); ?>
+				<?php
+
+				the_posts_pagination( array(
+					'mid_size' => 2,
+					'prev_text' => '&laquo; Previous',
+					'next_text' => 'Next &raquo;',
+				) );
+
+				?>
 			</div>
 			<div class="column two">
 				<!-- intentionally empty -->
