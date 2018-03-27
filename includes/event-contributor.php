78cc989bb1633bccd9011a5cb10d8473bf5431fd
Phil Cable <pcable@wsu.edu>
2018-03-27 11:55:28 -0700

    Remove capability handling provided by WP Event Calendar
    I'm operating on the assumption that there's good groundwork here,
    and I just don't understand well enough to leverage it.

--- includes/event-contributor.php
+++ includes/event-contributor.php
@@ -2,8 +2,30 @@
 
 namespace WSU\Events\Event_Contributor;
 
+remove_filter( 'map_meta_cap', 'wp_event_calendar_meta_caps', 10, 4 );
+
+add_filter( 'register_post_type_args', 'WSU\Events\Event_Contributor\filter_event_post_type_args', 10, 2 );
 add_action( 'admin_init', 'WSU\Events\Event_Contributor\add_role' );
 add_action( 'switch_theme', 'WSU\Events\Event_Contributor\remove_role' );
+
+/**
+ * Unsets the capability related arguments from the event post type.
+ *
+ * @since 0.2.4
+ *
+ * @param array  Arguments for registering a post type.
+ * @param string Post type key.
+ *
+ * @return array
+ */
+function filter_event_post_type_args( $args, $post_type ) {
+	if ( 'event' === $post_type ) {
+		unset( $args['capabilities'] );
+		unset( $args['capability_type'] );
+	}
+
+	return $args;
+}
 
 /**
  * Adds the Event Contributor role on theme activation.
